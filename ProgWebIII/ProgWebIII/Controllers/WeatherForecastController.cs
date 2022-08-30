712f65c5ebb36799548ea28d71cef1193d9424a8
Amanda Mantovani <amanda.mantovani@luizalabs.com>
2022-08-29 21:19:21 -0300

    Aula 2 ControllerBase ApiController Decorators

--- ProgWebIII/ProgWebIII/Controllers/WeatherForecastController.cs
+++ ProgWebIII/ProgWebIII/Controllers/WeatherForecastController.cs
@@ -4,6 +4,8 @@ namespace ProgWebIII.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -27,34 +29,65 @@ namespace ProgWebIII.Controllers
         }
 
         //https://localhost:7030/WeatherForecast GET
-        [HttpGet]
-        public List<WeatherForecast> Consultar(int index)
+        [HttpGet("/tempos/consultar")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        //public ActionResult<List<WeatherForecast>> Consultar([FromQuery] int index, string index2)
+        public ActionResult<List<WeatherForecast>> Consultar()
         {
-            return tempos;
+            return Ok(tempos);
+        }
+
+        //https://localhost:7030/WeatherForecast GET
+        [HttpGet("/tempo/{index}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<WeatherForecast> Consultar2(int index)
+        {
+            if (index > 4)
+            {
+                return NotFound();
+            }
+            return Ok(tempos[index]);
         }
 
         //https://localhost:7030/WeatherForecast POST
-        [HttpPost]
-        public WeatherForecast Inserir(WeatherForecast tempo)
+        [HttpPost("/tempo/cadastrar")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<WeatherForecast> Inserir(WeatherForecast tempo)
         {
             tempos.Add(tempo);
-            return tempo;
+            return StatusCode(201, tempo);
         }
 
         //https://localhost:7030/WeatherForecast PUT
-        [HttpPut]
-        public WeatherForecast Atualizar(int index, WeatherForecast tempo)
+        [HttpPut("/tempo/{index}/atualizar")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Atualizar(int index, WeatherForecast tempo)
         {
+            if (index >= tempos.Count || index < 0)
+            {
+                return NotFound();
+            }
+
             tempos[index] = tempo;
-            return tempos[index];
+            return NoContent();
         }
 
         //https://localhost:7030/WeatherForecast DELETE
-        [HttpDelete]
-        public List<WeatherForecast> Deletar(int index)
+        [HttpDelete("/tempo/{index}/deletar")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Deletar(int index)
         {
+            if (index >= tempos.Count || index < 0)
+            {
+                return NotFound();
+            }
+
             tempos.RemoveAt(index);
-            return tempos;
+            return NoContent();
         }
     }
 }
--- ProgWebIII/ProgWebIII/WeatherForecast.cs
+++ ProgWebIII/ProgWebIII/WeatherForecast.cs
@@ -1,13 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProgWebIII
 {
     public class WeatherForecast
     {
-        public DateTime Date { get; set; }
-
+        [Required(ErrorMessage = "Data é obrigatoria")]
+        public DateTime? Date { get; set; }
+        
+        [Range(-50, 50)]
+        [Required]
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+
+        [MaxLength(11, ErrorMessage = "Sumario deve conter até 11 caracteres")]
         public string? Summary { get; set; }
     }
 }
